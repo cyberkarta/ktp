a324eaf026f8f315dace8128d7ae97427e1f84de
aldykaj <45971773+aldykaj@users.noreply.github.com>
2022-06-10 15:30:15 +0700

    finish

--- app/api/controller.js
+++ app/api/controller.js
@@ -4,6 +4,9 @@ module.exports = {
         try {
             const {id} = req.params
             const data = await Ktp.findOne({ where: { nik: id } });
+            if(!data){
+                return res.status(404).json({message : "Data tidak ditemukan"})
+            }
             res.status(200).json({data : data})
         } catch (error) {
             res.status(500).json({message: error.message || `Internal Server Error`})
